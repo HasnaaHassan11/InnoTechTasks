095a6da62ed161582ad944bd081a7c36d3b57ca2
حسناء 31159874 <UG_31159874@ics.tanta.edu.eg>
2025-07-22 15:55:45 +0300

    Task1

---
+++ Week1/Backend/Week1/SummationFrom1ToN/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Please, Enter the maximum number.");
+int maxNumber = int.Parse(Console.ReadLine() ?? "0");
+if (maxNumber <= 0)
+{
+    Console.WriteLine("The number less than 1");
+    return;
+}
+
+#region Summation with for loop
+var watch = System.Diagnostics.Stopwatch.StartNew();
+int sumWithForLoop = 0;
+for (int i = 1; i <= maxNumber; i++)
+{
+    sumWithForLoop += i;
+}
+Console.WriteLine($"The sum of numbers from 1 to {maxNumber} using a for loop is: {sumWithForLoop}");
+watch.Stop();
+Console.WriteLine($"Time taken using for loop: {watch.ElapsedMilliseconds} ms");
+#endregion
+
+#region Summation with formula
+watch.Restart();
+int sumWithFormula = (maxNumber * (maxNumber + 1)) / 2;
+Console.WriteLine($"The sum of numbers from 1 to {maxNumber} using the formula is: {sumWithFormula}");
+watch.Stop();
+Console.WriteLine($"Time taken using formula: {watch.ElapsedMilliseconds} ms");
+#endregion
---
+++ Week1/Backend/Week1/WritingNumbersFrom1ToN/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+Console.WriteLine("Please, Enter the maximum number.");
+int maxNumber = int.Parse(Console.ReadLine() ?? "0");
+if (maxNumber <= 0)
+{
+    Console.WriteLine("The number less than 1");
+    return;
+}
+
+#region Writing numbers with string concatenation
+var watch = System.Diagnostics.Stopwatch.StartNew();
+string resultWithString = string.Empty;
+for (int i = 1; i <= maxNumber; i++)
+{
+    resultWithString += $"{i},";
+}
+resultWithString = resultWithString.TrimEnd(',');
+Console.WriteLine($"The numbers from 1 to {maxNumber} are:\n {resultWithString}");
+watch.Stop();
+Console.WriteLine($"Time taken using string concatenation: {watch.ElapsedMilliseconds} ms");
+#endregion
+
+#region Writing numbers with StringBuilder
+watch.Restart();
+var resultWithStringBuilder = new StringBuilder();
+for (int i = 1; i <= maxNumber; i++)
+{
+    resultWithStringBuilder.Append(i);
+    if (i < maxNumber)
+    {
+        resultWithStringBuilder.Append(",");
+    }
+}
+Console.WriteLine($"The numbers from 1 to {maxNumber} are:\n {resultWithStringBuilder}");
+watch.Stop();
+Console.WriteLine($"Time taken using StringBuilder: {watch.ElapsedMilliseconds} ms");
+#endregion
